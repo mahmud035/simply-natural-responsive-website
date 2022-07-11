49acc554503c464340e97b7cbce4b0313ede857c
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-11 10:42:10 +0600

    added media query for navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const listItems = document.querySelectorAll('.list-item');
+console.log(toggleButton, navbarLinks, listItems);
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
+
+listItems.forEach((item) => {
+  // console.log(item);
+  item.addEventListener('click', function () {
+    navbarLinks.classList.remove('active');
+  });
+});
